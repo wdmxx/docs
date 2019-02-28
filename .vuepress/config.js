2755a9104f73ef16e0cb3c7569daf5d151a90fef
wdm_xx_0704 <chaina152478@163.com>
2019-02-28 09:16:25 +0800

    test

---
+++ .vuepress/config.js
@@ -0,0 +1,21 @@
+// .vuepress/config.js
+module.exports = {
+  base: '/vue/',
+  title: '个人主页', 
+  description: 'wdm',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },                      // 根路径
+      { text: '首页',  link:'/testtest/'},
+    ],
+    sidebar: {
+      '/testtest/':[
+        '/testtest/head_test'
+      ],
+      //['/home2', 'home2自定义标题'],
+      //'/home3',
+      //'中文',
+    }
+  }
+
+}
